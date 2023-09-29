0967aae0cf6b8e2dc27d1dbb3b48ae668f59037f
hidellwind <lanapavlovaxx@gmail.com>
2023-09-30 01:37:56 +0300

    Добавьте файлы проекта.

---
+++ hw C# 4/Program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение:");
+        string sentence = Console.ReadLine();
+
+        string[] words = SplitSentence(sentence);
+        PrintWords(words);
+    }
+
+    static string[] SplitSentence(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        return words;
+    }
+
+    static void PrintWords(string[] words)
+    {
+        Console.WriteLine("Разделенные слова:");
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
