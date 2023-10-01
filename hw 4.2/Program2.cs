e7d0ab2e8bd717721c8f726b7bb86f7b32745972
hidellwind <lanapavlovaxx@gmail.com>
2023-10-01 16:48:17 +0300

    4-ое дз

---
+++ hw 4.2/Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение:");
+        string inputPhrase = Console.ReadLine();
+
+        string[] words = SplitWords(inputPhrase);
+        string reversedPhrase = ReverseWords(words);
+
+        Console.WriteLine("Предложение в рандомной последовательности:");
+        Console.WriteLine(reversedPhrase);
+    }
+
+    static string[] SplitWords(string inputPhrase)
+    {
+        string[] words = inputPhrase.Split(' ');
+        return words;
+    }
+
+    static string ReverseWords(string[] words)
+    {
+        Array.Reverse(words);
+        string reversedPhrase = string.Join(" ", words);
+        return reversedPhrase;
+    }
+}
